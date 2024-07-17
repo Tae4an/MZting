bb226289c97487ea0d22923b17acd69f33a5278f
TaeSan Choi <xotks7524@gmail.com>
2024-07-17 14:04:12 +0900

    first commit

---
+++ src/main/java/com/example/mzting/MztingApplication.java
@@ -0,0 +1,13 @@
+package com.example.mzting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MztingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MztingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mzting/MztingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mzting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MztingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
