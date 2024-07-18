8bb1ed9926936c903afd4cc2eab78a5d27b1e5d0
dmsqja <bum17822@naver.com>
2024-07-18 18:57:23 +0900

    Feat: react-router-dom

--- front/src/components/ProfileCard.js
+++ front/src/components/ProfileCard.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from '../styles/MainPage.module.css';
 
 const ProfileCard = ({ imageUrl, name, type, tags }) => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/chat');
+    };
+
     return (
-        <div className={styles.profileCard}>
+        <div className={styles.profileCard} onClick={handleClick}>
             <img src={imageUrl} alt={`Profile of ${name}`} className={styles.profileImage} />
             <div className={styles.profileInfo}>
                 <div className={styles.profileName}>{name}</div>
