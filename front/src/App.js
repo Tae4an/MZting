79efb5197c10d0396bbfb36dbcfd6e62c675be46
dmsqja <bum17822@naver.com>
2024-07-18 18:58:32 +0900

    Feat: LoginPage, Mainpage, ChatPage, HistoryListPage

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import { Layout } from "./components";
-import {ChatPage, ResultPage, MainPage, HistoryListPage} from "./pages";
+import { ChatPage, ResultPage, MainPage, HistoryListPage, LoginPage } from "./pages";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return (
-      <Layout>
-        <ResultPage />
-      </Layout>
-  );
+    return (
+        <Router>
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<LoginPage />} />
+                    <Route path="/main" element={<MainPage />} />
+                    <Route path="/result" element={<ResultPage />} />
+                    <Route path="/chat" element={<ChatPage />} />
+                    <Route path="/history" element={<HistoryListPage />} />
+                </Routes>
+            </Layout>
+        </Router>
+    );
 }
-
 
 export default App;
