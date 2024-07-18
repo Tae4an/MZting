3f342260012df6ff3a5bf936010d9b6ed3243ef2
dmsqja <bum17822@naver.com>
2024-07-18 18:57:52 +0900

    Feat: react-router-dom

--- front/src/pages/ChatPage.js
+++ front/src/pages/ChatPage.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import styles from '../styles/ChatPage.module.css';
 import { ChatBox } from '../components';
 
 const ChatPage = () => {
+    const location = useLocation();
+    const { imageUrl, name, type, tags } = location.state || {};
+
     return (
         <main className={styles.mainContainer}>
             <div className={styles.contentWrapper}>
-                <ChatBox />
+                <ChatBox imageUrl={imageUrl} name={name} type={type} tags={tags} />
             </div>
         </main>
     );
