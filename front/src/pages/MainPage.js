cd603b4fa4e8457e728884aeec3fdf14ed299426
dmsqja <bum17822@naver.com>
2024-07-22 16:34:39 +0900

    Modify ScrollIndicator and css

--- front/src/pages/MainPage.js
+++ front/src/pages/MainPage.js
@@ -11,22 +11,22 @@ import image5 from '../assets/image5.jpg';
 import image6 from '../assets/image6.jpg';
 
 const profileData = [
-    { image: image1, name: "이지은", type: "#INFJ", tags: "#배려심 #사려깊은 #내향적" },
-    { image: image2, name: "백지헌", type: "#ENFJ", tags: "#상냥 #공감 #사회적" },
-    { image: image3, name: "신세경", type: "#ISFJ", tags: "#친절함 #헌신적 #책임감" },
-    { image: image4, name: "고윤정", type: "#ISTP", tags: "#현실적 #실용적 #모험심" },
-    { image: image5, name: "한소희", type: "#INFP", tags: "#이상주의 #충실 #내향적" },
-    { image: image6, name: "유지민", type: "#ENTP", tags: "#도전적 #논쟁 #호기심" },
+    { id: 1, image: image1, name: "이지은", type: "#INFJ", tags: "#배려심 #사려깊은 #내향적" },
+    { id: 2, image: image2, name: "백지헌", type: "#ENFJ", tags: "#상냥 #공감 #사회적" },
+    { id: 3, image: image3, name: "신세경", type: "#ISFJ", tags: "#친절함 #헌신적 #책임감" },
+    { id: 4, image: image4, name: "고윤정", type: "#ISTP", tags: "#현실적 #실용적 #모험심" },
+    { id: 5, image: image5, name: "한소희", type: "#INFP", tags: "#이상주의 #충실 #내향적" },
+    { id: 6, image: image6, name: "유지민", type: "#ENTP", tags: "#도전적 #논쟁 #호기심" },
 ];
 
 const MainPage = () => {
-    const [scrollWidth, setScrollWidth] = useState('0%');
+    const [scrollPosition, setScrollPosition] = useState(0);
 
     const handleScroll = () => {
-        const scrollTop = document.documentElement.scrollTop;
-        const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-        const scrollPercentage = (scrollTop / scrollHeight) * 100;
-        setScrollWidth(`${scrollPercentage}%`);
+        const totalScrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        const currnetScrollPosition = window.scrollY;
+        const scrollIndicatorPosition = (currnetScrollPosition / totalScrollHeight) * (document.documentElement.clientHeight - 129);
+        setScrollPosition(scrollIndicatorPosition)
     };
 
     useEffect(() => {
@@ -45,10 +45,10 @@ const MainPage = () => {
             <hr className={styles.divider} />
             <div className={styles.profileGrid}>
                 {profileData.map((profile, index) => (
-                    <ProfileCard key={index} {...profile} />
+                    <ProfileCard key={profile.id} {...profile} />
                 ))}
             </div>
-            <div className={styles.scrollIndicator} style={{ '--scroll-width': scrollWidth }} />
+            <div className={styles.scrollIndicator} style={{ position: 'fixed', top: scrollPosition}}/>
         </div>
     );
 };
