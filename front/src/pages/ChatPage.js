df33c8d79ee78af34f6fb14553d167fd41f79e9d
dmsqja <bum17822@naver.com>
2024-07-24 17:51:22 +0900

    Feat: ChatPage에서 프로필 상세 모달 추가
    - useState 훅을 사용하여 showModal 상태 추가
    - handleProfileClick 함수 추가하여 프로필 클릭 시 모달 표시
    - handleCloseModal 함수 추가하여 모달 닫기 기능 구현
    - ChatBox 컴포넌트에 onProfileClick prop 전달하여 프로필 클릭 이벤트 처리
    - ProfileDetailModal 컴포넌트를 ChatPage에 추가하여 프로필 상세 정보 모달 구현

--- front/src/pages/ChatPage.js
+++ front/src/pages/ChatPage.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import styles from '../styles/ChatPage.module.css';
 import { ChatBox } from '../components';
+import { ProfileDetailModal } from "../components/ProfileDetailModal";
 
 const ChatPage = () => {
     const location = useLocation();
     const { image, name, type, tags } = location.state || {};
+    const [showModal, setShowModal] = useState(false);
+
+    const handleProfileClick = () => {
+        setShowModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
 
     return (
         <main className={styles.mainContainer}>
             <div className={styles.contentWrapper}>
-                <ChatBox image={image} name={name} type={type} tags={tags} />
+                <ChatBox
+                    image={image}
+                    name={name}
+                    type={type}
+                    tags={tags}
+                    onProfileClick={handleProfileClick}
+                />
             </div>
+            {showModal && (
+                <ProfileDetailModal
+                    show={showModal}
+                    onClose={handleCloseModal}
+                    profile={{ image: image, name, type, tags }}
+                />
+            )}
         </main>
     );
 };
