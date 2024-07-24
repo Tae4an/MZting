1bfc96526bcad05eb56d7ed54b0c635eb1ad5478
dmsqja <bum17822@naver.com>
2024-07-24 19:45:18 +0900

    feat: ChatBox.js에 이전 페이지로 이동하는 기능 추가
    - useNavigate 훅을 사용하여 이전 페이지로 이동하는 기능 추가
    - '<' 버튼 클릭 시 handleBackClick 함수 호출하여 navigate(-1) 실행

--- front/src/components/ChatBox.js
+++ front/src/components/ChatBox.js
@@ -1,36 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 import styles from '../styles/ChatBox.module.css';
 
-const ChatBox = ({ image, name }) => (
+const ChatBox = ({ image, name }) => {
+    const navigate = useNavigate();
 
+    const handleBackClick = () => {
+        navigate(-1); // 이전 페이지로 이동
+    };
 
-    <section className={styles.chatContainer}>
-        <header className={styles.chatHeader}>
-            <div className={styles.userInfo}>
-                <span className={styles.backbutton}>&lt;</span>
-                <img src={image} alt="User avatar" className={styles.avatar} />
-                <span className={styles.userName}>{name}</span>
+    return (
+        <section className={styles.chatContainer}>
+            <header className={styles.chatHeader}>
+                <div className={styles.userInfo}>
+                    <button
+                        className={styles.backbutton}
+                        onClick={handleBackClick}
+                        onKeyDown={(e) => { if (e.key === 'Enter') handleBackClick(); }}
+                        aria-label="Go back"
+                    >
+                        &lt;
+                    </button>
+                    <img src={image} alt="User avatar" className={styles.avatar} />
+                    <span className={styles.userName}>{name}</span>
+                </div>
+                <div className={styles.statusIndicator} />
+            </header>
+            <p className={styles.situationDescription}>
+                상황 설명 상황 설명 설명 상황 설명 상황 끄어엄 끄어어엄<br />
+                (ex 당신은 주선자의 소개를 통해 연락이 닿았습니다.)<br />
+            </p>
+            <div className={styles.messageContainer}>
+                <div className={styles.receivedMessage}>
+                    <img src={image} alt="Message avatar" className={styles.messageAvatar} />
+                    <div className={styles.message}>안녕하세요 이번에 소개받게 된 {name}(이)라고 합니다~</div>
+                </div>
+                <div className={styles.receivedMessage}>
+                    <img src={image} alt="Message avatar" className={styles.messageAvatar} />
+                    <div className={styles.message}>대화내용</div>
+                </div>
+                <div className={styles.messageSent}>내용대화</div>
             </div>
-            <div className={styles.statusIndicator} />
-        </header>
-        <p className={styles.situationDescription}>
-            상황 설명 상황 설명 설명 상황 설명 상황 끄어엄 끄어어엄<br />
-            (ex 당신은 주선자의 소개를 통해 연락이 닿았습니다.)<br />
-        </p>
-        <div className={styles.messageContainer}>
-            <div className={styles.receivedMessage}>
-                <img src={image} alt="Message avatar" className={styles.messageAvatar} />
-                <div className={styles.message}>안녕하세요 이번에 소개받게 된 {name}(이)라고 합니다~</div>
-            </div>
-            <div className={styles.receivedMessage}>
-                <img src={image} alt="Message avatar" className={styles.messageAvatar} />
-                <div className={styles.message}>대화내용</div>
-            </div>
-            <div className={styles.messageSent}>내용대화</div>
-        </div>
-    </section>
-);
+        </section>
+    );
+};
 
 ChatBox.propTypes = {
     image: PropTypes.string.isRequired,
