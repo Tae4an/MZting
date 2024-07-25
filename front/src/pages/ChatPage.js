258581e62328144f47e371592098d45cc81a117b
dmsqja <bum17822@naver.com>
2024-07-25 10:23:52 +0900

    REFACTOR: ChatPage 컴포넌트에서 ProfileDetailModal 렌더링 제거
    - ChatBox 컴포넌트에서 모달을 관리하도록 변경
    - 중복 모달 렌더링 문제 해결

--- front/src/pages/ChatPage.js
+++ front/src/pages/ChatPage.js
@@ -1,20 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import styles from '../styles/ChatPage.module.css';
-import { ChatBox, ProfileDetailModal } from '../components';
+import { ChatBox } from '../components';
 
 const ChatPage = () => {
     const location = useLocation();
     const { image, name, type, age, height, job, hobbies, tags, description } = location.state || {};
-    const [showModal, setShowModal] = useState(false);
-
-    const handleProfileClick = () => {
-        setShowModal(true);
-    };
-
-    const handleCloseModal = () => {
-        setShowModal(false);
-    };
 
     useEffect(() => {
         console.log('ChatPage loaded with state:', location.state);
@@ -26,17 +17,9 @@ const ChatPage = () => {
                 <ChatBox
                     image={image}
                     name={name}
-                    onProfileClick={handleProfileClick} // 핸들러 전달
+                    profileDetails={{ image, name, type, age, height, job, hobbies, tags, description }}
                 />
             </div>
-            {showModal && (
-                <ProfileDetailModal
-                    show={showModal}
-                    onClose={handleCloseModal}
-                    profile={{ image, name, type, age, height, job, hobbies, tags, description }}
-                    showChatButton={false} // "대화 시작하기" 버튼 숨기기
-                />
-            )}
         </main>
     );
 };
