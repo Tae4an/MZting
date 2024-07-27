b2b3e9bf65e88d428a364b72152c7e1585d41b53
Choi TaeSan <xotks7524@gmail.com>
2024-07-27 18:36:43 +0900

    Feat: Chat, Choice, Situation 리포지토리 추가

---
+++ src/main/java/com/example/mzting/repository/ChoiceRepository.java
@@ -0,0 +1,8 @@
+package com.example.mzting.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.awt.*;
+
+public interface ChoiceRepository extends JpaRepository<Choice, Integer> {
+}
