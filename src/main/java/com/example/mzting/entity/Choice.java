09772e7dcf63a2f7962b69a04342abd062e56e9a
Choi TaeSan <xotks7524@gmail.com>
2024-07-27 18:31:36 +0900

    Feat: Chat, Choice, Situation 엔티티 추가
    - 사용자, 프로필, 상황 관계를 포함한 Chat 엔티티 생성
    - 상황 및 다음 상황 연관 관계를 가진 Choice 엔티티 구현
    - Choice와 일대다 관계를 갖는 Situation 엔티티 개발
    - 모든 엔티티에 대한 적절한 JPA 어노테이션 설정
    - Chat 엔티티를 위한 SenderType 열거형 포함

---
+++ src/main/java/com/example/mzting/entity/Choice.java
@@ -0,0 +1,21 @@
+package com.example.mzting.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Choice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer choiceId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "situation_id")
+    private Situation situation;
+
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "next_situation_id")
+    private Situation nextSituation;
+
+}
---
+++ src/main/java/com/example/mzting/entity/Situation.java
@@ -0,0 +1,19 @@
+package com.example.mzting.entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Entity
+public class Situation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer situationId;
+
+    private Integer chapter;
+    private String place;
+
+    @OneToMany(mappedBy = "situation", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Collection<Choice> choices = new ArrayList<>();
+}
