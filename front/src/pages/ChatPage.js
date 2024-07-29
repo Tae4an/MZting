7830c10f4281ce25bdc155b0420b04c42c52364d
dmsqja <bum17822@naver.com>
2024-07-29 20:56:16 +0900

    Refactor: 코드 정리 및 불필요한 import 제거
    - `ChatPage` 컴포넌트에서 불필요한 import 및 useEffect 초기 로드 로깅 정리
    - `useState` 훅을 통해 메시지 상태 관리
    - `handleSendMessage` 함수 개선하여 메시지 전송 및 응답 처리

--- front/src/pages/ChatPage.js
+++ front/src/pages/ChatPage.js
@@ -1,15 +1,13 @@
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import styles from '../styles/ChatPage.module.css';
 import { ChatBox } from '../components';
 import { sendMessage } from '../services';
 
-
 const ChatPage = () => {
     const location = useLocation();
     const { image, name, type, age, height, job, hobbies, tags, description } = location.state || {};
     const mbti = type.replace('#', ''); // type에서 #을 제거하여 mbti 추출
-
 
     useEffect(() => {
         console.log('ChatPage loaded with state:', location.state);
@@ -22,7 +20,7 @@ const ChatPage = () => {
             const newMessage = { content, isSent: true };
             setMessages(prevMessages => [...prevMessages, newMessage]);
 
-            const response = await sendMessage(content,mbti);
+            const response = await sendMessage(content, mbti);
             const responseMessage = {
                 content: response,
                 isSent: false,
@@ -33,6 +31,7 @@ const ChatPage = () => {
             console.error('Error sending message:', error);
         }
     };
+
     return (
         <main className={styles.mainContainer}>
             <div className={styles.contentWrapper}>
