220a61b4678d9a93e7661901651885176f41348d
dmsqja <bum17822@naver.com>
2024-07-30 17:23:32 +0900

    Refactor: 불필요한 컴포넌트 제거 및 파일 정리
    - HistoryCard 및 HistoryList 컴포넌트를 불필요한 목록에서 제거
    - 전체 코드에서 가독성과 유지보수성을 향상시키기 위해 사용되지 않는 모듈 제거
    - 나머지 컴포넌트의 가져오기 및 내보내기 구조를 정리하여 코드의 일관성 유지

--- front/src/components/index.js
+++ front/src/components/index.js
@@ -1,7 +1,5 @@
 import { LoginForm } from "./LoginForm";
 import { AdColumn } from "./AdColumn";
-import { HistoryCard } from "./HistoryCard";
-import { HistoryList } from "./HistoryList";
 import { Layout } from "./Layout";
 import { ProfileCard } from "./ProfileCard";
 import { ChatBox } from "./ChatBox";
@@ -16,8 +14,6 @@ import { TypingIndicator } from "./TypingIndicator";
 export {
     LoginForm,
     AdColumn,
-    HistoryCard,
-    HistoryList,
     Layout,
     ProfileCard,
     ChatBox,
