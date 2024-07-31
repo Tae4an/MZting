98b3950ec821a6e4ffa50496d99a1ac8d7b404d0
dmsqja <bum17822@naver.com>
2024-07-31 11:29:39 +0900

    Style: TypingIndicator 스타일 업데이트
    - 배경 색상을 어두운 색상에서 밝은 색상으로 변경 (#3D3D56 -> #ffffff)
    - 이미지에 그림자 추가 (box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1))

--- front/src/components/TypingIndicator.js
+++ front/src/components/TypingIndicator.js
@@ -4,7 +4,7 @@ import typing from '../assets/TypingEllipse.gif';
 const TypingIndicator = () => {
     return (
         <div style={{ display: 'flex', alignItems: 'center', marginLeft: '10px' }}>
-            <img src={typing} alt="Typing..." style={{ width: '65px', height: '45px', backgroundColor: '#3D3D56', borderRadius: '20px', objectFit: 'cover' }} />
+            <img src={typing} alt="Typing..." style={{ width: '65px', height: '45px', backgroundColor: '#ffffff', borderRadius: '20px', objectFit: 'cover', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)' }} />
         </div>
     );
 };
