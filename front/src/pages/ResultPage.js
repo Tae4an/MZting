36c35106c3d3bc93c7ce968cd683e5fee99ff3b8
dmsqja <bum17822@naver.com>
2024-07-31 14:32:28 +0900

    Refactor: ResultPage.js 리팩토링 및 스타일 개선
    - 불필요한 useEffect 및 useState 제거
    - 전체 구조 및 스타일링 개선
    - page 클래스 사용으로 전체 레이아웃 조정
    - header 섹션 재구성 및 버튼 아이콘 추가
    - profileCard, actionButton, scoreCard, reviewButton 스타일 수정
    - 대화 결과 타이틀 및 프로필 정보 업데이트

--- front/src/pages/ResultPage.js
+++ front/src/pages/ResultPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import styles from '../styles/ResultPage.module.css'
 import image2 from '../assets/Images/image2.jpg'
@@ -7,35 +7,31 @@ const ResultPage = () => {
     const navigate = useNavigate();
 
     return (
-        <div className={styles.container}>
-            <div className={styles.content}>
-                <div className={styles.mainColumn}>
-                    <div className={styles.conversationContainer}>
-                        <div className={styles.header}>
-                            <div className={styles.backButton} onClick={() => navigate(-1)}>&lt;</div>
-                            <img src={image2} className={styles.profileImage} alt="Profile" />
-                            <div className={styles.title}>백지헌과의 대화 결과</div>
-                        </div>
-                        <div className={styles.profileCard}>
-                            <div className={styles.profileHeader}>
-                                <div className={styles.profileName}>이름 : 백지헌</div>
-                                <div className={styles.personalityType}>#INFJ</div>
-                            </div>
-                            <div className={styles.profileDetails}>
-                                나이 : 25<br />
-                                키 : 170<br />직업 : 마케터<br />
-                                취미 : TED 강연 참여, 토론, 연극 보기, 새로운 사람 만나기
-                            </div>
-                        </div>
-                        <div className={styles.actionButton}>대화 로그 보기</div>
-                        <div className={styles.scoreCard}>
-                            <div className={styles.compatibilityScore}>궁합 점수 : ㅇㅇ</div>
-                            <div className={styles.finalScore}>최종 호감도 : ㅇㅇ</div>
-                        </div>
-                        <div className={styles.reviewButton}>대화 후기 보기</div>
-                    </div>
+        <div className={styles.page}>
+            <div className={styles.header}>
+                <button className={styles.backButton} onClick={() => navigate(-1)}>
+                    <i className="bi bi-arrow-left"></i>
+                </button>
+                <img src={image2} className={styles.profileImage} alt="Profile" />
+                <div className={styles.title}>#ENFJ 와의 대화 결과</div>
+            </div>
+            <div className={styles.profileCard}>
+                <div className={styles.profileHeader}>
+                    <div className={styles.profileName}>이름 : 백지헌</div>
+                    <div className={styles.personalityType}>#ENFJ</div>
+                </div>
+                <div className={styles.profileDetails}>
+                    나이 : 25<br />
+                    키 : 170<br />직업 : 마케터<br />
+                    취미 : TED 강연 참여, 토론, 연극 보기, 새로운 사람 만나기
                 </div>
             </div>
+            <div className={styles.actionButton}>대화 로그 보기</div>
+            <div className={styles.scoreCard}>
+                <div className={styles.compatibilityScore}>궁합 점수 : ㅇㅇ</div>
+                <div className={styles.finalScore}>최종 호감도 : ㅇㅇ</div>
+            </div>
+            <div className={styles.reviewButton}>대화 후기 보기</div>
         </div>
     );
 }
