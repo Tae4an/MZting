94cbc17ac19dbd38b068a08c7a17a85f28f1e3dc
TaeSan Choi <xotks7524@gmail.com>
2024-07-31 17:42:12 +0900

    Styles: 프로필 카드 태그 디자인 수정

--- front/src/components/ProfileCard.js
+++ front/src/components/ProfileCard.js
@@ -3,14 +3,20 @@ import PropTypes from 'prop-types';
 import styles from '../styles/MainPage.module.css';
 
 const ProfileCard = ({ image, name, type, tags, onClick }) => {
+    const tagList = tags.split('#').filter(tag => tag.trim() !== '');
+
     return (
         <button className={styles.profileCard} onClick={onClick}>
             <img src={image} alt={name} className={styles.profileImage} />
-            <div className={styles.profileInfo}>
-                <div className={styles.profileName}>{name}</div>
-                <div className={styles.profileType}>{type}</div>
+            <div className={styles.profileHeader}>
+                <span className={styles.profileName}>{name}</span>
+                <span className={styles.mbtiTag}>#{type.replace('#', '')}</span>
             </div>
-            <div className={styles.profileTags}>{tags}</div>
+            <div className={styles.profileTags}>
+                {tagList.map((tag, index) => (
+                    <span key={index} className={styles.tag}>#{tag.trim()}</span>
+                ))}
+            </div>
         </button>
     );
 };
