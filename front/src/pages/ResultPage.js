0daee38b400a3db2b885de35afe52030fdc56a68
dmsqja <bum17822@naver.com>
2024-07-31 12:12:15 +0900

    Feat: ResultPage 컴포넌트 업데이트
    - useNavigate 훅을 사용하여 뒤로 가기 버튼 클릭 시 이전 페이지로 이동 기능 추가
    - 프로필 이미지 경로를 하드코딩된 URL에서 로컬 이미지 파일(image2)로 변경
    - 프로필 세부 정보 및 레이아웃 수정
    - 코드 스타일 및 포맷 개선
    
    이 변경으로 ResultPage에서 뒤로 가기 기능이 추가되고, 프로필 이미지가 로컬 파일로 업데이트됨.

--- front/src/pages/ResultPage.js
+++ front/src/pages/ResultPage.js
@@ -1,26 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from '../styles/ResultPage.module.css'
+import image2 from '../assets/Images/image2.jpg'
 
 const ResultPage = () => {
+    const navigate = useNavigate();
+
     return (
         <div className={styles.container}>
             <div className={styles.content}>
                 <div className={styles.mainColumn}>
                     <div className={styles.conversationContainer}>
                         <div className={styles.header}>
-                            <div className={styles.backButton}>&lt;</div>
-                            <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/ac6c300228a69c666dd5a5766d6b2c3f926f191b74049c3a0e32ea7f07bd0c70?apiKey=49c1e3d53b81482bb61cc4f10fd5a261&" className={styles.profileImage} alt="Profile" />
-                            <div className={styles.title}>이지은과의 대화 결과</div>
+                            <div className={styles.backButton} onClick={() => navigate(-1)}>&lt;</div>
+                            <img src={image2} className={styles.profileImage} alt="Profile" />
+                            <div className={styles.title}>백지헌과의 대화 결과</div>
                         </div>
                         <div className={styles.profileCard}>
                             <div className={styles.profileHeader}>
-                                <div className={styles.profileName}>이름 : 이지은</div>
+                                <div className={styles.profileName}>이름 : 백지헌</div>
                                 <div className={styles.personalityType}>#INFJ</div>
                             </div>
                             <div className={styles.profileDetails}>
-                                나이 : 23<br />
-                                키 : 165 직업 : 작가<br />
-                                취미 : 독서 , 일기, 글쓰기, 음악 감상
+                                나이 : 25<br />
+                                키 : 170<br />직업 : 마케터<br />
+                                취미 : TED 강연 참여, 토론, 연극 보기, 새로운 사람 만나기
                             </div>
                         </div>
                         <div className={styles.actionButton}>대화 로그 보기</div>
