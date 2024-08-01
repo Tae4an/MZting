61640365ddcaff16a61bdbcc2570c03688edabbd
TaeSan Choi <xotks7524@gmail.com>
2024-08-01 13:49:40 +0900

    Feat: ChatRoomId 추가

--- src/main/java/com/example/mzting/dto/UserMessage.java
+++ src/main/java/com/example/mzting/dto/UserMessage.java
@@ -1,12 +1,12 @@
 package com.example.mzting.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class UserMessage {
     private String message;
     private String mbti;
-
-    // getter and setter
+    private Long chatRoomId;
 }
-
