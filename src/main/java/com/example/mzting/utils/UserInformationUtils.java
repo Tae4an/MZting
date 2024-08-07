ec6dfa50ce6524ef5e76e5ddfe83827f05e453b9
minkinghub <kp141@naver.com>
2024-08-07 17:59:31 +0900

    REFACTOR : Oauth 부분 수정

--- src/main/java/com/example/mzting/utils/UserInformationUtils.java
+++ src/main/java/com/example/mzting/utils/UserInformationUtils.java
@@ -1,9 +1,11 @@
 package com.example.mzting.utils;
 
+import com.example.mzting.entity.User;
 import com.example.mzting.repository.UserRepository;
 import com.example.mzting.dto.UserInformationDTO;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,15 +17,18 @@ public class UserInformationUtils {
         this.userRepository = userRepository;
     }
 
-    public UserInformationDTO getCurrentUserInfo() throws UnauthorizedException {
+    public UserInformationDTO getCurrentUserInfo() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.isAuthenticated()) {
-            String username = authentication.getName();
-            Long uid = userRepository.findIdByEmail(username);
-            return new UserInformationDTO(uid, username);
-        } else {
-            throw new UnauthorizedException("User is not authenticated2");
+        if (authentication == null || !(authentication.getPrincipal() instanceof OAuth2User)) {
+            throw new UnauthorizedException("User not authenticated");
         }
+
+        OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
+        String email = oauth2User.getAttribute("email");
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UnauthorizedException("User not found"));
+
+        return new UserInformationDTO(user.getId(), user.getUsername(), user.getEmail());
     }
 
     public static class UnauthorizedException extends RuntimeException {
