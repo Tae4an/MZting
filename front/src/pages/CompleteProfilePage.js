1b7a525d0971b3a186736272e740bfafdbe88232
dmsqja <bum17822@naver.com>
2024-08-21 11:03:26 +0900

    Refactor: CompleteProfilePage 수정
    - 로고 이미지로 변경
    - 글꼴 지정

--- front/src/pages/CompleteProfilePage.js
+++ front/src/pages/CompleteProfilePage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import styles from '../styles/CompleteProfilePage.module.css';
+import MZting_logo from '../assets/Images/MZting_logo.png';
 
 const mbtiOptions = [
     "INTJ", "INTP", "ENTJ", "ENTP",
@@ -53,10 +54,10 @@ const CompleteProfilePage = () => {
     return (
         <div className={styles.completeProfileContainer}>
             <form className={styles.completeProfileForm} onSubmit={handleSubmit}>
-                <h1 className={styles.completeProfileTitle}>mzting</h1>
+                <img src={MZting_logo} className={styles.signupLogo} alt="Signup Logo"/>
                 {error && <p className={styles.error}>{error}</p>}
                 <div className={styles.inputGroup}>
-                    <label htmlFor="nickname">Nickname</label>
+                    <label htmlFor="nickname" className={styles.font}>Nickname</label>
                     <input
                         type="text"
                         id="nickname"
@@ -64,29 +65,32 @@ const CompleteProfilePage = () => {
                         value={profile.nickname}
                         onChange={handleChange}
                         required
+                        className={styles.font}
                     />
                 </div>
                 <div className={styles.selectAge}>
-                    <label htmlFor="age">Age</label>
+                    <label htmlFor="age" className={styles.font}>Age</label>
                     <select
                         id="age"
                         name="age"
                         value={profile.age}
                         onChange={handleChange}
                         required
+                        className={styles.font}
                     >
                         <option value="" disabled>나이 선택</option>
                         {generateAgeOptions()}
                     </select>
                 </div>
                 <div className={styles.selectGender}>
-                    <label htmlFor="gender">Gender</label>
+                    <label htmlFor="gender" className={styles.font}>Gender</label>
                     <select
                         id="gender"
                         name="gender"
                         value={profile.gender}
                         onChange={handleChange}
                         required
+                        className={styles.font}
                     >
                         <option value="" disabled>성별 선택</option>
                         <option value="male">남성</option>
@@ -94,13 +98,14 @@ const CompleteProfilePage = () => {
                     </select>
                 </div>
                 <div className={styles.selectMBTI}>
-                    <label htmlFor="mbti">MBTI</label>
+                    <label htmlFor="mbti" className={styles.font}>MBTI</label>
                     <select
                         id="mbti"
                         name="mbti"
                         value={profile.mbti}
                         onChange={handleChange}
                         required
+                        className={styles.font}
                     >
                         <option value="" disabled>MBTI 선택</option>
                         {mbtiOptions.map((option) => (
@@ -108,7 +113,7 @@ const CompleteProfilePage = () => {
                         ))}
                     </select>
                 </div>
-                <button type="submit" className={styles.completeProfileButton}>Complete Profile</button>
+                <button type="submit" className={`${styles.completeProfileButton} ${styles.font}`}>Complete Profile</button>
             </form>
         </div>
     );
